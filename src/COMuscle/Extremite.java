582a9fa758eac103f9614cfa8d3f32462078205c
theolaf <theo.lafforgue@gmail.com>
2016-12-09 19:02:11 +0100

    Update Extremite.java

--- src/COMuscle/Extremite.java
+++ src/COMuscle/Extremite.java
@@ -3,4 +3,22 @@ package COMuscle;
 public class Extremite {
   private Jonction saJonction;
   private Segment sonSegment;
+  
+  /* GETTERS ET SETTERS */
+  
+  public Jonction getSaJonction() {
+    return saJonction;
+  }
+  
+  public void setSaJonction(Jonction saJonction_) {
+    this.saJonction = saJonction_;
+  }
+  
+  public Segment getSonSegment() {
+    return sonSegment;
+  }
+  
+  public void setSonSegment(Segment sonSegment_) {
+    this.sonSegment = sonSegment_;
+  }
 }
