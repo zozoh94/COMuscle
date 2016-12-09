13968515e65bb427a0f72f1dc2bc6df354e9adb8
theolaf <theo.lafforgue@gmail.com>
2016-12-09 16:56:31 +0100

    Update Vehicule.java

--- src/COMuscle/Vehicule.java
+++ src/COMuscle/Vehicule.java
@@ -1,8 +1,32 @@
 package COMuscle;
 
-public class Vehicule {
+public abstract class Vehicule {
     protected Emplacement emplacement;
-
+    protected int place; // coordonnées précises sur l'emplacement
+    protected int vitesse;
+    
+    public void deplacement() {
+        this.place += this.vitesse; // à modif pour gérer les cas où la caisse change d'emplacement
+    }
+    
+    /* GETTERS ET SETTERS */
+    
+    public int getVitesse() {
+        return vitesse;
+    }
+    
+    public void setVitesse(int vitesse_) {
+        this.vitesse = vitesse_;
+    }
+    
+    public int getPlace() {
+        return place;
+    }
+    
+    public void setPlace(int place_) {
+        this.place = place_;
+    }
+    
     public Emplacement getEmplacement() {
         return emplacement;
     }
