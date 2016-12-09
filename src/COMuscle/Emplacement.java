ba1ae7ebd375b6f3407c88f35f9c7e5ee6abaf44
theolaf <theo.lafforgue@gmail.com>
2016-12-09 17:16:58 +0100

    Update Emplacement.java

--- src/COMuscle/Emplacement.java
+++ src/COMuscle/Emplacement.java
@@ -2,8 +2,17 @@ package COMuscle;
 
 public abstract class Emplacement {
   protected int longueur;
+  protected Capteur sonCapteur;
   
   /* GETTERS ET SETTERS */
+  
+  public Capteur getSonCapteur(){
+    return sonCapteur;
+  }
+  
+  public void setSonCapteur(Capteur capteur) {
+    this.sonCapteur = capteur;
+  }
   
   public int getLongueur() {
     return longueur;
