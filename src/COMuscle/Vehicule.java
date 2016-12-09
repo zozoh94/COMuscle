3c89e1396357ff3eaa1ddd2c89693958b2b589bc
theolaf <theo.lafforgue@gmail.com>
2016-12-09 18:07:19 +0100

    Update Vehicule.java

--- src/COMuscle/Vehicule.java
+++ src/COMuscle/Vehicule.java
@@ -2,6 +2,25 @@ package COMuscle;
 
 public class Vehicule {
     protected Emplacement emplacement;
+    protected ArrayList<Vehicule> sesVehicules;
+    
+    /* GETTERS ET SETTERS */
+    
+    public ArrayList<Vehicule> getSesVehicules() {
+        return sesVehicules;
+    }
+    
+    public void setSesVehicules(ArrayList<Vehicule> sesVehicules_) {
+        this.sesVehicules = sesVehicules_;
+    }
+    
+    public void addVehicule(Vehicule vehicule) {
+        this.sesVehicules.add(vehicule);
+    }
+
+    public void removeVehicule(Vehicule vehicule) {
+        this.sesVehicules.remove(vehicule);
+    }
 
     public Emplacement getEmplacement() {
         return emplacement;
