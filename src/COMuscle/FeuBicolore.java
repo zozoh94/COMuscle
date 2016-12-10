0124c3814d907aeb639299a4074f337a9a73acec
Raihane SIDQUI <raihane.s@hotmail.fr>
2016-12-10 22:32:35 +0100

    Add files via upload

--- src/COMuscle/FeuBicolore.java
+++ src/COMuscle/FeuBicolore.java
@@ -2,4 +2,12 @@ package COMuscle;
 
 public class FeuBicolore extends Feux {
 	BicolorEnum etat;
+	
+	public FeuBicolore(BicolorEnum etat_) {
+		etat=etat_;
+	}
+	
+	public String toString() {
+		return etat.toString();
+	}
 }
