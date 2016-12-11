3802e88a6fd33581ef679d905908d89e4e81ac23
insow <beneccli@gmail.com>
2016-12-11 02:56:23 +0100

    Ajout de sesVehicules dans Emplacement
    La mise à jour de sesVehicules est faite automatiquement si la position
    du vehicule est donnée directement dans le constructeur (j'ai pas géré
    les autres cas de modification de position)

--- src/COMuscle/Emplacement.java
+++ src/COMuscle/Emplacement.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public abstract class Emplacement {
 	protected int longueur;
-	ArrayList<File> files;
+	protected ArrayList<File> files;
+	protected ArrayList<Vehicule> sesVehicules;
 	
 	public Emplacement() {
 		files = new ArrayList<File>();
@@ -33,5 +34,21 @@ public abstract class Emplacement {
 	
 	public void setFiles(ArrayList<File> files) {
 		this.files = files;
+	}
+	
+	public ArrayList<Vehicule> getSesVehicules() {
+		return sesVehicules;
+	}
+	    
+	public void setSesVehicules(ArrayList<Vehicule> sesVehicules_) {
+		this.sesVehicules = sesVehicules_;
+	}
+	    
+	public void addVehicule(Vehicule vehicule) {
+		this.sesVehicules.add(vehicule);
+	}
+	
+	public void removeVehicule(Vehicule vehicule) {
+		this.sesVehicules.remove(vehicule);
 	}
 }
--- src/COMuscle/Vehicule.java
+++ src/COMuscle/Vehicule.java
@@ -17,6 +17,7 @@ public abstract class Vehicule {
     public Vehicule (Position position) {
 		this();
 		this.position = position;
+		this.position.getEmplacement().addVehicule(this);
     }
     
     public void deplacement() {
